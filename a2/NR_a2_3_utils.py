c7f5d21c196816912adc0423f557cc2bc2f8ba51
Chrisvb95 <christiaanvanbuchem@gmail.com>
2019-05-16 23:18:03 +0200

    finished runge cuta function

--- a2/NR_a2_3_utils.py
+++ a2/NR_a2_3_utils.py
@@ -11,21 +11,6 @@ def k_calc(h,f,t,x1,x2,xn):
     k4 = h * f(t+h,x1+k3,x2+k3)
     return xn+1/6*k1+1/3*k2+1/3*k3+1/6*k4
 
-def runge_kutta2nd(x1_0,x2_0,t,f1,f2,h=0.01):
-    #Implementaiton of the Runge-Kutta method for ODE integration
-    # x0,y0 are the starting values and f is the ode()
-
-    t = np.arange(1,t,h)
-    x1n,x2n = x1_0,x2_0
-    x1_out = np.zeros(len(t))
-    for i in range(len(t)):
-        x1_new = k_calc(h,f1,t[i],x1n,x2n,x1n)
-        x2_new = k_calc(h,f2,t[i],x1n,x2n,x2n)
-        x1_out[i] = x1_new
-        x1n,x2n = x1_new,x2_new
-
-    return np.sum(x1_out)*h
-
 def runge_kutta(x0,y0,f,xmax,h=0.0001):
     #Implementaiton of the Runge-Kutta method for ODE integration
     # x0,y0 are the starting values and f is the ode()
@@ -42,4 +27,31 @@ def runge_kutta(x0,y0,f,xmax,h=0.0001):
 
     return np.sum(y_out)*h
 
-def 
+def k_calc2nd(h,f,g,t,x1,x2):
+    # Support function for runge_kutta method (for 2nd order ODEs)
+    k1 = h * f(t,x1,x2)
+    l1 = h * g(t,x1,x2)
+    k2 = h * f(t+0.5*h,x1+0.5*k1,x2+0.5*l1)
+    l2 = h * g(t+0.5*h,x1+0.5*k1,x2+0.5*l1)
+    k3 = h * f(t+0.5*h,x1+0.5*k2,x2+0.5*k2)
+    l3 = h * g(t+0.5*h,x1+0.5*k2,x2+0.5*k2)
+    k4 = h * f(t+h,x1+k3,x2+k3)
+    l4 = h * g(t+h,x1+k3,x2+k3)
+
+    x1_new = x1+1/6*(k1+2*k2+2*k3+k4)
+    x2_new = x2+1/6*(l1+2*l2+2*l3+l4)
+
+    return x1_new, x2_new
+#end k_calc2nd()
+
+def runge_kutta2nd(x1_0,x2_0,t0,t,f,g,h=0.01):
+    #Implementaiton of the Runge-Kutta method for ODE integration
+    # x0,y0 are the starting values and f is the ode()
+    t = np.arange(t0,t+h,h)
+    #print(t)
+    x1n,x2n = x1_0,x2_0
+    x1_out = np.zeros(len(t))
+    for i in range(len(t)):
+        x1n,x2n = k_calc2nd(h,f,g,t[i],x1n,x2n)
+        x1_out[i] = x1n 
+    return np.sum(x1_out)*h,x1_out
