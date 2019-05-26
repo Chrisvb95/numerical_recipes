a2b371a8aa0ae6deb6bf39db72ec37b589bb4c91
Chrisvb95 <christiaanvanbuchem@gmail.com>
2019-05-26 22:41:17 +0200

    'Exercise'

--- a2/a2_5.py
+++ a2/a2_5.py
@@ -1,7 +1,48 @@
 #a2_5.py
-mport numpy as np
+import numpy as np
 import sys
 import matplotlib.pyplot as plt
+
+def NGP(p,N):
+
+    mesh = np.zeros((N,N,N))
+    for i in range(len(p[0])):
+        x,y,z = np.round(p[:,i])%N
+        mesh[int(x)][int(y)][int(z)] += 1
+        
+    return mesh
+
+def CiC(p,N):
+
+    mesh = np.zeros((N,N,N))
+    for i in range(len(p[0])):
+        w = np.zeros(8)
+        x,y,z = np.round(p[:,i])%N
+        dx,dy,dz = x-p[0,i],y-p[1,i],z-p[2,i]
+        sx,sy,sz = np.sign(dx),np.sign(dy),np.sign(dz)
+        dx,dy,dz = np.abs(dx),np.abs(dy),np.abs(dz)
+        # Calculating all of the weights
+        w[0] = (1-dx)*(1-dy)*(1-dz)
+        w[1] = (dx)*(1-dy)*(1-dz)
+        w[2] = (1-dx)*(dy)*(1-dz)
+        w[3] = (1-dx)*(1-dy)*(dz)
+        w[4] = (dx)*(dy)*(dz-1)
+        w[5] = (dx)*(1-dy)*(dz)
+        w[6] = (1-dx)*(dy)*(dz)
+        w[7] = (dx)*(dy)*(dz)        
+        # Assigning the weights
+        mesh[np.int(x)-1][np.int(y)-1][np.int(z)-1] += w[0]
+        mesh[np.int(x+sx)-1][np.int(y)-1][np.int(z)-1] += w[1]
+        mesh[np.int(x)-1][np.int(y+sy)-1][np.int(z)-1] += w[2]
+        mesh[np.int(x)-1][np.int(y)-1][np.int(z+sz)-1] += w[3]
+        mesh[np.int(x+sx)-1][np.int(y+sy)-1][np.int(z)-1] += w[4]
+        mesh[np.int(x+sx)-1][np.int(y)-1][np.int(z+sz)-1] += w[5]
+        mesh[np.int(x)-1][np.int(y+sy)-1][np.int(z+sz)-1] += w[6]   
+        mesh[np.int(x+sx)-1][np.int(y+sy)-1][np.int(z+sz)-1] += w[7]
+
+    return mesh
+
+
 
 def fft1D(x,Nj,start=0,step=1):
     #if Nj%2 > 0:
